53801bc6aa4519df96dcef64c649f17f273ae4f8
ToriMarques <vitoriamarques.developer@gmail.com>
2025-10-05 23:43:45 -0300

    Deploy com banco de dados na turso v2

--- test-turso.js
+++ test-turso.js
@@ -2,7 +2,7 @@
 const { createClient } = require('@libsql/client');
 
 const client = createClient({
-  url: process.env.TURSO_DATABASE_URL,
+  url: process.env.DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
